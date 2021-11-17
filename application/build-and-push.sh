4235cce713b2de1d80494f2a37f47eea371f1352
Stefan Kürzeder <info@stivik.me>
2021-11-17 11:57:07 +0100

    Added Demo

---
+++ application/build-and-push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+export IMAGE_NAME="acr0dev0thromis20210de.azurecr.io/example:$1"
+echo "Building image $IMAGE_NAME"
+
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME .
+
+echo "Pushing Docker image..."
+docker push $IMAGE_NAME
---
+++ application/go.mod
@@ -0,0 +1,3 @@
+module github.com/StiviiK/helm-handson
+
+go 1.17
