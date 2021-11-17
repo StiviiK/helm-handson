53ce332d2e7b71416fa21c018a9c43699df5befb
Stefan Kürzeder <info@stivik.me>
2021-11-17 15:21:54 +0100

    Cleanup repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Helm Hands-On Training
+
+## Sources
+
+Application: [/application](/application)  
+Helm Chart: [/helm-chart](/helm-chart)  
+Documentation: [/documentation](/documentation)  
+Scripts: [/scripts](/scripts)  
--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -5,7 +5,7 @@ export IMAGE_NAME="acr0dev0thromis20210de.azurecr.io/example:$1"
 echo "Building image $IMAGE_NAME"
 
 echo "Building Docker image..."
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME application
 
 echo "Pushing Docker image..."
 docker push $IMAGE_NAME
