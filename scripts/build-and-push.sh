45f833959a85e34d31d32ce83023ae83d3efd732
Stefan Kürzeder <info@stivik.me>
2021-11-17 16:39:35 +0100

    Added PowerPoint presentation

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-export IMAGE_NAME="acr0dev0thromis20210de.azurecr.io/example:$1"
-echo "Building image $IMAGE_NAME"
+export TAG=${1:-latest}
+export IMAGE_NAME="acr0dev0thromis20210de.azurecr.io/example"
 
-echo "Building Docker image..."
-docker build -t $IMAGE_NAME application
+echo -e "\033[36mBuilding Docker image $IMAGE_NAME:$TAG..."
+docker build -t "$IMAGE_NAME:$TAG" application
 
-echo "Pushing Docker image..."
-docker push $IMAGE_NAME
+echo -e "\033[36mPushing Docker image..."
+docker push "$IMAGE_NAME:$TAG"
