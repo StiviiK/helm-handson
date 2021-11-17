419f91fc941c6b16388b4f2462c0ba757683394e
Stefan Kürzeder <info@stivik.me>
2021-11-17 19:02:45 +0100

    Update helm chart

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+export REGISTRY="acr0dev0thromis20210de.azurecr.io"
+export IMAGE_NAME="${REGISTRY}/example"
 export TAG=${1:-latest}
-export IMAGE_NAME="acr0dev0thromis20210de.azurecr.io/example"
+
+echo -e "\033[36mLogging in into the Registry..."
+az acr login --name ${REGISTRY}
 
 echo -e "\033[36mBuilding Docker image $IMAGE_NAME:$TAG..."
 docker build -t "$IMAGE_NAME:$TAG" application
